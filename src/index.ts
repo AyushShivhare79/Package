c4aba20a925e11966884cefc826cc3abdcee9a3e
AyushShivhare79 <ayushshivhare51@gmail.com>
2026-01-29 12:50:38 +0530

    Init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.use("/api/v1");
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
